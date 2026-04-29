0611e2526106b20b61c0ce633531142bd6e50704
amug-smth <amoghishaan@gmail.com>
2026-04-29 15:32:19 +0530

    Add files via upload

---
+++ break,pass,continue.py
@@ -0,0 +1,17 @@
+word="codingal"
+for i in word:
+    if i == "a" or i=="A":
+        break
+    print(i)
+print("Pass")
+word="codingal"
+for i in word:
+    if i == "a" or i=="A":
+        pass
+    print(i)
+print("Continue")
+word="codingal"
+for i in word:
+    if i == "a" or i=="A":
+        continue
+    print(i)
